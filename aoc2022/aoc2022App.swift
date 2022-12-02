294e51d5828a4f3e7bd00c634be0c940476a253c
Ben Liset <ben.liset@coyote.com>
2022-12-02 09:25:17 -0600

    Initial Commit

---
+++ aoc2022/aoc2022App.swift
@@ -0,0 +1,17 @@
+//
+//  aoc2022App.swift
+//  aoc2022
+//
+//  Created by Ben Liset on 12/2/22.
+//
+
+import SwiftUI
+
+@main
+struct aoc2022App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
